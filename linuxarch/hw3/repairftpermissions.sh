3081d18665b7251c6acbc4312180b9d38e4f95fb
Mark Okulov <mvokulov@gmail.com>
2021-07-14 18:15:57 +0000

    rewrite repairftpermissions

--- linuxarch/hw3/repairftpermissions.sh
+++ linuxarch/hw3/repairftpermissions.sh
@@ -4,5 +4,12 @@ chmod 2775 /var/ftp
 for d in /var/ftp/*/; do
 	name=`echo $d | cut -d/ -f4`
 	chown -R $name:ftp-admin /var/ftp/$name
-	chmod 2770 -R /var/ftp/$name
+	for element in `find /var/ftp/$name`; do
+		if [ -d $element ]; then
+			chmod 2775 $element;
+		else
+			chmod  664 $element;
+		fi
+	done
+#	chmod 2770 -R /var/ftp/$name
 done
